5910f49cfc806486444a3bcb417d4bb5e28025a3
Aqsa Naseer <arzoosultani123456@gmail.com>
2024-03-17 00:00:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Task:4
+console.log(`Albert Einstein once said, "I have no special talent, I am only passionately curious." `);
+// Task:5
+let famousPerson = "Albert Einstein";
+let message = `${famousPerson} once said, "I have no specoial talent, I am only passionately curious."`;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Task:4
+
+console.log(`Albert Einstein once said, "I have no special talent, I am only passionately curious." `)
+
+// Task:5
+
+let famousPerson: string = "Albert Einstein";
+let message : string = `${famousPerson} once said, "I have no specoial talent, I am only passionately curious."`
+console.log(message)
